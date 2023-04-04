2aed6586c4407e4e28ef071588419604845419cc
grantgariepy <grantgariepy@protonmail.com>
2023-04-04 14:18:53 -0600

    logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,9 @@
 		class="bg-fixed w-screen h-screen bg-cover bg-center bg-no-repeat -mb-24"
 		style="background-image:url('images/out1.jpg')"
 	>
-		<h1 class="text-6xl w-screen text-center font-bold">OUTSIDE</h1>
+		<h1 class=" tracking-widest logo text-8xl w-screen text-center antialiased text-white">
+			dontblend.in
+		</h1>
 	</div>
 	<div
 		class="bg-fixed w-screen h-screen mix-blend-screen bg-cover bg-top bg-no-repeat -mb-24"
