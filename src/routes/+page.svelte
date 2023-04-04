c5d88946df9509f4a1984ac5efdacb4fa3baadf5
grantgariepy <grantgariepy@protonmail.com>
2023-04-04 10:05:59 -0600

    more image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,16 @@
 <div>
-	<div class="bg-fixed w-screen h-screen" id="first">
+	<div
+		class="bg-fixed w-screen h-screen bg-cover bg-center bg-no-repeat -mb-24"
+		style="background-image:url('images/out1.jpg')"
+	>
 		<h1 class="text-6xl w-screen text-center font-bold">OUTSIDE</h1>
-		<!-- <div class="absolute inset-x-0 bottom-3 h-24" /> -->
 	</div>
-	<div class="bg-fixed w-screen h-screen mix-blend-lighten" id="second">
-		<!-- <div class="absolute inset-x-0 bottom-3 h-24" /> -->
-	</div>
-	<!-- <div class="bg-fixed w-screen h-screen" id="second">
-		<div class=" h-24 inset-0 top-0" id="blend" />
-		<div class="bg-white mix-blend-difference">hi</div>
-	</div>
-	<div class="bg-fixed w-screen h-screen" id="third">
-		<div class=" h-24 top-0 mix-blend-multiply bg-white" id="blend" />
-	</div> -->
+	<div
+		class="bg-fixed w-screen h-screen mix-blend-screen bg-cover bg-top bg-no-repeat -mb-24"
+		style="background-image:url('images/out2.webp')"
+	/>
+	<div
+		class="bg-fixed w-screen h-screen mix-blend-screen bg-center bg-cover bg-no-repeat"
+		style="background-image:url('images/out3.webp')"
+	/>
 </div>
-<!-- <img class="w-screen mix-blend-lighten" src="images/out2.webp" alt="" /> -->
-<!-- <img class="w-screen sticky z-0 top-0 inset-0" src="images/street.jpg" alt="" />
-	<img class="w-screen top-0 z-10 inset-0" src="images/street.jpg" alt="" /> -->
