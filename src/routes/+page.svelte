f72d618148b8e754305afbd713c630b711ff0716
grantgariepy <grantgariepy@protonmail.com>
2023-04-03 20:25:32 -0600

    i have cracked the code hahaha

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<div class="bg-fixed w-screen h-screen" id="first">
+		<!-- <div class="absolute inset-x-0 bottom-3 h-24" /> -->
+	</div>
+	<div class="bg-fixed w-screen h-screen mix-blend-lighten" id="second">
+		<!-- <div class="absolute inset-x-0 bottom-3 h-24" /> -->
+	</div>
+	<!-- <div class="bg-fixed w-screen h-screen" id="second">
+		<div class=" h-24 inset-0 top-0" id="blend" />
+		<div class="bg-white mix-blend-difference">hi</div>
+	</div>
+	<div class="bg-fixed w-screen h-screen" id="third">
+		<div class=" h-24 top-0 mix-blend-multiply bg-white" id="blend" />
+	</div> -->
+</div>
+<!-- <img class="w-screen mix-blend-lighten" src="images/out2.webp" alt="" /> -->
+<!-- <img class="w-screen sticky z-0 top-0 inset-0" src="images/street.jpg" alt="" />
+	<img class="w-screen top-0 z-10 inset-0" src="images/street.jpg" alt="" /> -->
