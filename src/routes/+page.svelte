61e74086321723b47f87185c7fdd708a6d4b17e3
grantgariepy <grantgariepy@protonmail.com>
2023-04-04 18:50:54 -0600

    logo and font stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,42 @@
 <div>
+	<!-- first -->
 	<div
-		class="bg-fixed w-screen h-screen bg-cover bg-center bg-no-repeat -mb-24"
+		class="bg-fixed w-screen h-screen bg-cover bg-center bg-no-repeat -mb-16"
 		style="background-image:url('images/out1.jpg')"
 	>
-		<h1 class=" tracking-widest logo text-8xl w-screen text-center antialiased text-white">
-			dontblend.in
+		<h1 class="tracking-widest early-edition text-8xl w-screen text-center antialiased ">
+			HALCYON
 		</h1>
 	</div>
+	<!-- second -->
 	<div
-		class="bg-fixed w-screen h-screen mix-blend-screen bg-cover bg-top bg-no-repeat -mb-24"
+		class="bg-fixed w-screen h-screen mix-blend-screen bg-cover bg-top bg-no-repeat -mb-16"
 		style="background-image:url('images/out2.webp')"
 	/>
+	<!-- third -->
 	<div
-		class="bg-fixed w-screen h-screen mix-blend-screen bg-center bg-cover bg-no-repeat"
+		class="bg-fixed w-screen mix-blend-screen bg-center bg-cover bg-no-repeat "
+		id="third"
 		style="background-image:url('images/out3.webp')"
+	>
+		<div class="">
+			<div class="">
+				<p class="knockout text-white text-9xl tracking-widest font-bold pl-5">IDEAS 5</p>
+			</div>
+			<div class="">
+				<p class="text-white">hi</p>
+			</div>
+			<!-- <p class="text-white">hi</p> -->
+		</div>
+	</div>
+	<!-- fourth -->
+	<div
+		class="bg-fixed w-screen h-screen mix-blend-screen bg-center bg-cover bg-no-repeat  bg-green-500"
 	/>
 </div>
+
+<style>
+	#third {
+		height: 105vh;
+	}
+</style>
