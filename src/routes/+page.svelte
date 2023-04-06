fbc6a25851571acbf035df87d4b6aa2618d93fea
grantgariepy <grantgariepy@protonmail.com>
2023-04-06 09:17:26 -0600

    css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div>
 	<!-- first -->
 	<div
-		class="bg-fixed w-screen h-screen bg-cover bg-center bg-no-repeat -mb-16"
+		class="bg-fixed w-screen h-screen bg-cover bg-center bg-no-repeat -mb-12"
 		style="background-image:url('images/out1.jpg')"
 	>
 		<h1 class="tracking-widest early-edition text-8xl w-screen text-center antialiased ">
@@ -10,29 +10,31 @@
 	</div>
 	<!-- second -->
 	<div
-		class="bg-fixed w-screen h-screen mix-blend-screen bg-cover bg-top bg-no-repeat -mb-16"
+		class="bg-fixed w-screen h-[102vh] mix-blend-screen bg-cover bg-top bg-no-repeat -mb-24"
 		style="background-image:url('images/out2.webp')"
 	/>
 	<!-- third -->
 	<div
-		class="bg-fixed w-screen mix-blend-screen bg-center bg-cover bg-no-repeat "
+		class="bg-fixed w-screen h-[110vh] mix-blend-screen bg-center bg-cover bg-no-repeat "
 		id="third"
 		style="background-image:url('images/out3.webp')"
 	>
 		<div class="h-screen flex flex-col justify-center">
 			<div class="flex  justify-start">
-				<p
+				<a
+					href=""
 					class="knockout text-white text-4xl md:text-9xl lg:text-[11rem] tracking-wider font-bold md:pl-12 pl-5 md:pb-12 pb-14 "
 				>
 					IDEAS 5
-				</p>
+				</a>
 			</div>
 			<div class="flex md:justify-end justify-center">
-				<p
-					class="early-edition tracking-wider lg:text-7xl md:text-4xl text-xl text-black bg-white w-11/12 md:w-1/2 md:mr-20 justify-end flex md:pr-10 md:pb-8 md:pt-4 px-3 pb-4 pt-3"
+				<a
+					href=""
+					class="early-edition tracking-wider font-bold lg:text-7xl md:text-4xl text-xl text-black bg-white w-11/12 md:w-1/2 md:mr-20 justify-end flex md:pr-10 md:pb-9 md:pt-4 px-3 pb-4 pt-3 hover:bg-red-400 cursor-pointer"
 				>
-					Grant Gariepy for Halcyon
-				</p>
+					Grant Gariepy for halcyon
+				</a>
 			</div>
 			<!-- <p class="text-white">hi</p> -->
 		</div>
@@ -42,9 +44,3 @@
 		class="bg-fixed w-screen h-screen mix-blend-screen bg-center bg-cover bg-no-repeat  bg-green-500"
 	/>
 </div>
-
-<style>
-	#third {
-		height: 110vh;
-	}
-</style>
