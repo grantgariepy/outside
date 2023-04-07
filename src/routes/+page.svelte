290e6f2fdd2b6ec234203d74a8d68e14fb7e94b5
grantgariepy <grantgariepy@protonmail.com>
2023-04-06 18:30:35 -0600

    more sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,25 +39,68 @@
 		</div>
 	</div>
 	<!-- fourth -->
-	<div class="h-[110vh]">
-		<div class="flex flex-row">
+	<div>
+		<div class="flex flex-row justify-start ">
 			<div
-				class="out4 h-[110vh] w-1/2 md:bg-left-top bg-center bg-cover bg-fixed -mt-20 -mb-20 bg-no-repeat"
+				class=" out4 h-[120vh] w-1/2 bg-fixed -mt-24 -mb-24 bg-no-repeat mix-blend-screen"
 				style="background-image:url('images/out4.webp')"
-			>
-				<!-- <img
-				class="w-1/2 fixed inset-0 -z-10 h-screen object-cover object-top -mt-16"
-				src="images/out4.webp"
-				alt=""
-			/> -->
+			/>
+			<div class="out4-form bg-zinc-900 w-1/2 -mb-12 -mt-12">
+				<div>
+					<p class="text-white">hi</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- fifth -->
+	<div
+		class="bg-fixed w-screen h-[150vh] bg-center bg-cover bg-no-repeat -mb-24"
+		id="third"
+		style="background-image:url('images/out5.jpg')"
+	>
+		<div class="h-screen flex flex-col justify-center ">
+			<div class="flex justify-start pt-96">
+				<a
+					href=""
+					class="knockout text-white text-4xl md:text-9xl lg:text-[11rem] tracking-wider font-bold md:pl-12 pl-5 md:pb-12 pb-14 "
+				>
+					BOMBDEUX
+				</a>
+			</div>
+
+			<div class="flex justify-end">
+				<p
+					class="text-white lg:w-1/5 w-max mx-6 lg:ml-0 text-sm md:text-xl  tracking-wide  md:mr-[33vw] pb-6"
+				>
+					A calm, relaxed and nearly-bombproof 21st century take on a jean. Cut high and wide for a
+					loose, but not too loose, DGAF vibe. Built with Bombtwill, a custom developed fabric that
+					is lighter yet stronger than denim, with a whole lot of structure.
+				</p>
+			</div>
+			<div class="flex md:justify-end justify-center">
+				<a
+					href=""
+					class="early-edition tracking-wider font-bold lg:text-7xl md:text-4xl text-xl text-black bg-white w-11/12 md:w-1/2 md:mr-20 justify-end flex md:pr-10 md:pb-9 md:pt-4 px-3 pb-4 pt-3 hover:bg-red-400 cursor-pointer"
+				>
+					expected spring
+				</a>
 			</div>
 		</div>
 	</div>
 </div>
+<!-- sixth -->
+<div
+	class="bg-fixed w-screen h-[130vh] mix-blend-screen bg-center bg-cover bg-no-repeat "
+	id="third"
+	style="background-image:url('images/out6.webp')"
+/>
 
 <style>
 	.out4 {
 		background-position: 0 10%;
-		background-size: 50%;
+		background-size: 50vw;
+	}
+	.out4-form {
+		clip-path: polygon(5% 0, 100% 0, 100% 98%, 96% 100%, 0 100%, 0 2%);
 	}
 </style>
