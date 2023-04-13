1561d8180755d9c671d10c87a6d48eb201d0d972
grantgariepy <grantgariepy@protonmail.com>
2023-04-13 16:18:47 -0600

    pls wrk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <div class="desktop">
 	<!-- first -->
 	<div
-		class="out1 md:bg-fixed w-screen h-screen bg-cover bg-center bg-no-repeat -mb-12"
+		class="out1 bg-fixed w-screen h-screen bg-cover bg-center bg-no-repeat md:-mb-12"
 		style="background-image:url('images/out1.jpg')"
 	>
 		<h1 class="tracking-widest early-edition text-8xl w-screen text-center antialiased">HALCYON</h1>
 	</div>
 	<!-- second -->
 	<div
-		class="out2 md:bg-fixed w-screen h-screen mix-blend-screen bg-cover bg-top bg-no-repeat -mb-24"
+		class="out2 bg-fixed w-screen h-screen mix-blend-screen bg-cover bg-top bg-no-repeat md:-mb-24"
 		style="background-image:url('images/out2.jpg')"
 	/>
 	<!-- third -->
@@ -34,22 +34,52 @@
 		</div>
 	</div>
 	<!-- fourth -->
-	<div>
-		<div class="flex flex-row justify-start">
+	<div class="md:flex md:flex-row h-[200vh] md:h-screen">
+		<div
+			class="out4 h-screen md:h-[106vh] bg-cover w-full md:w-1/2 bg-fixed md:-mt-24 md:-mb-24 bg-no-repeat mix-blend-screen"
+			style="background-image:url('images/out4.jpg')"
+		/>
+		<div class="w-2/3 md:w-1/2 h-screen md:flex md:flex-col md:justify-center">
 			<div
-				class="out4 h-[120vh] w-1/2 bg-fixed -mt-24 -mb-24 bg-no-repeat mix-blend-screen"
-				style="background-image:url('images/out4.jpg')"
-			/>
-			<div class="out4-form bg-zinc-900 w-1/2 -mb-12 -mt-12">
-				<div>
-					<p class="text-white">hi</p>
+				class="out4-form absolute -mt-28 inset-x-0 md:relative bg-zinc-900 md:w-full md:h-screen md:-mb-12"
+			>
+				<div class="h-[90vh] flex flex-col justify-center">
+					<div class="flex justify-start">
+						<span
+							class="knockout text-white text-4xl md:text-9xl lg:text-[11rem] tracking-wider font-bold md:pl-12 pl-5 md:pb-12 pb-14"
+						>
+							IDEAS 5
+						</span>
+					</div>
+					<div class="flex md:justify-end justify-center">
+						<span
+							class="early-edition tracking-wider font-bold lg:text-7xl md:text-4xl text-xl text-black bg-white w-11/12 md:w-1/2 md:mr-20 justify-end flex md:pr-10 md:pb-9 md:pt-4 px-3 pb-4 pt-3 hover:bg-red-400 cursor-pointer"
+						>
+							Grant Gariepy for halcyon
+						</span>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+	<!-- <div>
+		<div class="md:flex md:flex-row md:justify-start">
+			<div
+				class="out4 h-[200vh] md:h-[106vh] bg-cover w-full md:w-1/2 bg-fixed md:-mt-24 md:-mb-24 bg-no-repeat mix-blend-screen"
+				style="background-image:url('images/out4.jpg')"
+			/>
+			<div class="h-1/2 flex flex-col justify-center">
+				<div class="out4-form md:relative bg-zinc-900 md:w-1/2 md:h-screen md:-mb-12 md:-mt-12">
+					<div>
+						<p class="text-white">hi</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> -->
 	<!-- fifth -->
 	<div
-		class="bg-fixed w-screen h-[150vh] bg-center bg-cover bg-no-repeat -mb-24"
+		class="bg-fixed w-screen h-[150vh] bg-center bg-cover bg-no-repeat md:-mb-24 -mt-72 -mb-24"
 		id="third"
 		style="background-image:url('images/out5.jpg')"
 	>
@@ -68,10 +98,10 @@
 				>
 					A calm, relaxed and nearly-bombproof 21st century take on a jean. Cut high and wide for a
 					loose, but not too loose, DGAF vibe. Built with Bombtwill, a custom developed fabric that
-					is lighter yet stronger than denim, with a whole lot of structure.
+					is lighter yet strr than denim, with a whole lot of structure.
 				</p>
 			</div>
-			<div class="flex md:justify-end justify-center">
+			<div class="flex md:justifyonge-end justify-center">
 				<span
 					class="early-edition tracking-wider font-bold lg:text-7xl md:text-4xl text-xl text-black bg-white w-11/12 md:w-1/2 md:mr-20 justify-end flex md:pr-10 md:pb-9 md:pt-4 px-3 pb-4 pt-3 hover:bg-red-400 cursor-pointer"
 				>
@@ -82,9 +112,7 @@
 	</div>
 	<!-- sixth -->
 	<div
-		class="bg-fixed w-screen h-[130vh] mix-blend-screen bg-center bg-cover bg-no-repeat"
-		id="third"
-		style="background-image:url('images/out6.jpg')"
+		class="out6 bg-fixed w-screen h-[130vh] mix-blend-screen bg-center-center bg-cover bg-no-repeat md:-mt-12"
 	/>
 </div>
 
@@ -179,17 +207,38 @@
 	.mobile {
 		display: none;
 	}
+	.out6 {
+		background-image: url('images/out6.jpg') !important;
+	}
 	@media only screen and (max-width: 600px) {
-		.desktop {
+		/* 	.desktop {
 			display: none;
 		}
 		.mobile {
 			display: block;
+		}*/
+		.out4 {
+			background-position: 50% !important;
+			background-size: auto 100vh !important;
+		}
+		.out6 {
+			background-image: url('images/out6-mobile.jpg') !important;
+		}
+	}
+	@media only screen and (max-width: 1700px) {
+		/* 	.desktop {
+			display: none;
+		}
+		.mobile {
+			display: block;
+		}*/
+		.out4 {
+			background-size: auto 100vh !important;
 		}
 	}
 	.out4 {
 		background-position: 0 10%;
-		background-size: 70vw;
+		background-size: 50%;
 	}
 	.out4-form {
 		clip-path: polygon(5% 0, 100% 0, 100% 98%, 96% 100%, 0 100%, 0 2%);
