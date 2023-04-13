bec1c76d461d20b7910ab2819ad320bb7f175f01
grantgariepy <grantgariepy@protonmail.com>
2023-04-13 15:03:00 -0600

    mobile test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,7 +37,7 @@
 	<div>
 		<div class="flex flex-row justify-start">
 			<div
-				class=" out4 h-[120vh] w-1/2 bg-fixed -mt-24 -mb-24 bg-no-repeat mix-blend-screen"
+				class="out4 h-[120vh] w-1/2 bg-fixed -mt-24 -mb-24 bg-no-repeat mix-blend-screen"
 				style="background-image:url('images/out4.jpg')"
 			/>
 			<div class="out4-form bg-zinc-900 w-1/2 -mb-12 -mt-12">
@@ -87,11 +87,106 @@
 		style="background-image:url('images/out6.jpg')"
 	/>
 </div>
+<div class="mobile">
+	<!-- section 1 -->
+	<div class="mobile-1 -mb-12">
+		<img src="images/out1.jpg" alt="" class="object-cover h-screen w-auto" />
+		<h1
+			class="absolute inset-x-0 top-0 tracking-widest early-edition text-8xl w-screen text-center antialiased"
+		>
+			HALCYON
+		</h1>
+	</div>
+	<!-- SECTION 2 -->
+	<div class="mobile-2 mix-blend-screen -mb-24">
+		<img src="images/out2.jpg" alt="" class="object-cover h-screen w-auto" />
+	</div>
+	<!-- SECTION 3 -->
+	<div class="mobile-3 mix-blend-screen -mb-24">
+		<div class="absolute inset-x-0 h-screen flex flex-col justify-center">
+			<div class="flex justify-start">
+				<span
+					class="knockout text-white text-4xl md:text-9xl lg:text-[11rem] tracking-wider font-bold md:pl-12 pl-5 md:pb-12 pb-14"
+				>
+					IDEAS 5
+				</span>
+			</div>
+			<div class="flex md:justify-end justify-center">
+				<span
+					class="early-edition tracking-wider font-bold lg:text-7xl md:text-4xl text-xl text-black bg-white w-11/12 md:w-1/2 md:mr-20 justify-end flex md:pr-10 md:pb-9 md:pt-4 px-3 pb-4 pt-3 hover:bg-red-400 cursor-pointer"
+				>
+					Grant Gariepy for halcyon
+				</span>
+			</div>
+		</div>
+		<img src="images/out3.jpg" alt="" class="object-cover h-screen w-auto" />
+	</div>
+	<!-- SECTION 4 -->
+	<div class="mobile-4 mix-blend-screen h-screen -mb-12">
+		<div class="absolute inset-x-0 flex flex-col justify-center">
+			<div class="out4-form bg-zinc-900 w-1/2">
+				<div>
+					<form action="">
+						<input class="input" type="text" />
+						<input class="input" type="text" />
+						<input class="input" type="text" />
+					</form>
+				</div>
+			</div>
+		</div>
+		<img src="images/out4.jpg" alt="" class="object-cover h-screen w-auto" />
+	</div>
+	<!-- SECTION 5 -->
+	<div class="mobile-5 mix-blend-screen h-screen">
+		<div class="absolute inset-x-0 flex flex-col justify-center">
+			<div class="flex justify-start pt-96">
+				<span
+					class="knockout text-white text-4xl md:text-9xl lg:text-[11rem] tracking-wider font-bold md:pl-12 pl-5 md:pb-12 pb-14"
+				>
+					BOMBDEUX
+				</span>
+			</div>
+
+			<div class="flex justify-end">
+				<p
+					class="text-white lg:w-1/5 w-max mx-6 lg:ml-0 text-sm md:text-xl tracking-wide md:mr-[33vw] pb-6"
+				>
+					A calm, relaxed and nearly-bombproof 21st century take on a jean. Cut high and wide for a
+					loose, but not too loose, DGAF vibe. Built with Bombtwill, a custom developed fabric that
+					is lighter yet stronger than denim, with a whole lot of structure.
+				</p>
+			</div>
+			<div class="flex md:justify-end justify-center">
+				<span
+					class="early-edition tracking-wider font-bold lg:text-7xl md:text-4xl text-xl text-black bg-white w-11/12 md:w-1/2 md:mr-20 justify-end flex md:pr-10 md:pb-9 md:pt-4 px-3 pb-4 pt-3 hover:bg-red-400 cursor-pointer"
+				>
+					expected spring
+				</span>
+			</div>
+		</div>
+		<img src="images/out5.jpg" alt="" class="object-cover h-screen w-auto" />
+	</div>
+	<!-- SECTION 6 -->
+	<div class="mobile-6 mix-blend-screen">
+		<img src="images/out6-mobile.jpg" alt="" class=" object-cover object-left h-screen w-auto" />
+	</div>
+</div>
 
 <style>
+	.mobile {
+		display: none;
+	}
+	@media only screen and (max-width: 600px) {
+		.desktop {
+			display: none;
+		}
+		.mobile {
+			display: block;
+		}
+	}
 	.out4 {
 		background-position: 0 10%;
-		background-size: 50vw;
+		background-size: 70vw;
 	}
 	.out4-form {
 		clip-path: polygon(5% 0, 100% 0, 100% 98%, 96% 100%, 0 100%, 0 2%);
