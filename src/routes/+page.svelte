f45b21694179c05221b27a0f0e0c7f288cdbfd83
grantgariepy <grantgariepy@protonmail.com>
2023-04-13 13:02:15 -0600

    jpg and different css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,42 @@
 <div>
 	<!-- first -->
 	<div
-		class="bg-fixed w-screen h-screen z-10 bg-cover bg-center bg-no-repeat -mb-12"
+		class="out1 bg-fixed w-screen h-screen z-10 bg-cover bg-center bg-no-repeat -mb-12"
 		style="background-image:url('images/out1.jpg')"
 	>
-		<h1 class="tracking-widest early-edition text-8xl w-screen text-center antialiased ">
-			HALCYON
-		</h1>
+		<h1 class="tracking-widest early-edition text-8xl w-screen text-center antialiased">HALCYON</h1>
 	</div>
 	<!-- second -->
 	<div
 		class="bg-fixed w-screen h-[102vh] z-10 mix-blend-screen bg-cover bg-top bg-no-repeat -mb-24"
-		style="background-image:url('images/out2.webp')"
+		style="background-image:url('images/out2.jpg')"
 	/>
 	<!-- third -->
 	<div
-		class="bg-fixed w-screen h-[120vh] z-10 mix-blend-screen bg-center bg-cover bg-no-repeat "
+		class="bg-fixed w-screen h-[120vh] z-10 mix-blend-screen bg-center bg-cover bg-no-repeat"
 		id="third"
 		style="background-image:url('images/out3.webp')"
 	>
 		<div class="h-screen flex flex-col justify-center">
-			<div class="flex  justify-start">
-				<a
-					href=""
-					class="knockout text-white text-4xl md:text-9xl lg:text-[11rem] tracking-wider font-bold md:pl-12 pl-5 md:pb-12 pb-14 "
+			<div class="flex justify-start">
+				<span
+					class="knockout text-white text-4xl md:text-9xl lg:text-[11rem] tracking-wider font-bold md:pl-12 pl-5 md:pb-12 pb-14"
 				>
 					IDEAS 5
-				</a>
+				</span>
 			</div>
 			<div class="flex md:justify-end justify-center">
-				<a
-					href=""
+				<span
 					class="early-edition tracking-wider font-bold lg:text-7xl md:text-4xl text-xl text-black bg-white w-11/12 md:w-1/2 md:mr-20 justify-end flex md:pr-10 md:pb-9 md:pt-4 px-3 pb-4 pt-3 hover:bg-red-400 cursor-pointer"
 				>
 					Grant Gariepy for halcyon
-				</a>
+				</span>
 			</div>
 		</div>
 	</div>
 	<!-- fourth -->
 	<div>
-		<div class="flex flex-row justify-start ">
+		<div class="flex flex-row justify-start">
 			<div
 				class=" out4 h-[120vh] w-1/2 bg-fixed -mt-24 -mb-24 bg-no-repeat mix-blend-screen"
 				style="background-image:url('images/out4.webp')"
@@ -58,19 +54,18 @@
 		id="third"
 		style="background-image:url('images/out5.jpg')"
 	>
-		<div class="h-screen flex flex-col justify-center ">
+		<div class="h-screen flex flex-col justify-center">
 			<div class="flex justify-start pt-96">
-				<a
-					href=""
-					class="knockout text-white text-4xl md:text-9xl lg:text-[11rem] tracking-wider font-bold md:pl-12 pl-5 md:pb-12 pb-14 "
+				<span
+					class="knockout text-white text-4xl md:text-9xl lg:text-[11rem] tracking-wider font-bold md:pl-12 pl-5 md:pb-12 pb-14"
 				>
 					BOMBDEUX
-				</a>
+				</span>
 			</div>
 
 			<div class="flex justify-end">
 				<p
-					class="text-white lg:w-1/5 w-max mx-6 lg:ml-0 text-sm md:text-xl  tracking-wide  md:mr-[33vw] pb-6"
+					class="text-white lg:w-1/5 w-max mx-6 lg:ml-0 text-sm md:text-xl tracking-wide md:mr-[33vw] pb-6"
 				>
 					A calm, relaxed and nearly-bombproof 21st century take on a jean. Cut high and wide for a
 					loose, but not too loose, DGAF vibe. Built with Bombtwill, a custom developed fabric that
@@ -78,24 +73,29 @@
 				</p>
 			</div>
 			<div class="flex md:justify-end justify-center">
-				<a
-					href=""
+				<span
 					class="early-edition tracking-wider font-bold lg:text-7xl md:text-4xl text-xl text-black bg-white w-11/12 md:w-1/2 md:mr-20 justify-end flex md:pr-10 md:pb-9 md:pt-4 px-3 pb-4 pt-3 hover:bg-red-400 cursor-pointer"
 				>
 					expected spring
-				</a>
+				</span>
 			</div>
 		</div>
 	</div>
 </div>
 <!-- sixth -->
 <div
-	class="bg-fixed w-screen h-[130vh] mix-blend-screen bg-center bg-cover bg-no-repeat "
+	class="bg-fixed w-screen h-[130vh] mix-blend-screen bg-center bg-cover bg-no-repeat"
 	id="third"
 	style="background-image:url('images/out6.webp')"
 />
 
 <style>
+	.out1 {
+		background-size: cover;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		-o-background-size: cover;
+	}
 	.out4 {
 		background-position: 0 10%;
 		background-size: 50vw;
